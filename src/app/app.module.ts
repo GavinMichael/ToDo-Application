1c4c434fdaf84165a24a0ebb4ac4e555a6779800
GavinMichael <gavin.michael.ftw@gmail.com>
2017-08-22 03:21:43 +0400

    Added major component & Router functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
+import { NavComponent } from './nav/nav.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CompletedTodoComponent } from './completed-todo/completed-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TodoComponent,
+    NavComponent,
+    NotFoundComponent,
+    CompletedTodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'active',
+        component: TodoComponent
+      },      
+      {
+        path: 'completed',
+        component: CompletedTodoComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
