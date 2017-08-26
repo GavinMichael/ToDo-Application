af0855a4eaebe428503a954a995efe2992bc7950
GavinMichael <gavin.michael.ftw@gmail.com>
2017-08-27 00:32:12 +0400

    Added delete todo

--- src/app/completed-todo/completed-todo.component.ts
+++ src/app/completed-todo/completed-todo.component.ts
@@ -50,8 +50,22 @@ export class CompletedTodoComponent implements OnInit {
       }
   }
 
-  // add a delete btn
-  // mark as not complete btn
-  // remove unneeded css styles from completed-todo.component.css
+  // Deleting a todo
+  deleteTodo(todo) {
+    // get index of the todo being deleted
+    let index = this.completedTodosArray.indexOf(todo);
+    // remove that from the view
+    this.completedTodosArray.splice(index, 1);
+    // Delete http request sent to service
+    let id = todo._id;
+    this.service.deleteTodo(id)
+      .subscribe(Response => {
+        this.toastr.success('Deleted successfully');     
+      }),
+      Error => {
+        console.log(Error);  
+        this.toastr.error('Error! Failed to delete');      
+      }
+  }
 
 }
